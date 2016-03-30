a13cdbbff52b745005d69b7667f788eb20614169
Einharch <Einharch@gmail.com>
2016-03-30 23:55:29 +0900

    Showing how to have UI elements over/under some effects

--- DynamicSizeParticlesDemo/GameScene.swift
+++ DynamicSizeParticlesDemo/GameScene.swift
@@ -12,19 +12,34 @@ class GameScene: SKScene
 {
     //var particlesNode = ParticlesNode()
     var particlesNode : ParticlesNode!
+    var wall =  SKSpriteNode()
 
     func loadParticles(type type: ParticlesType)
     {
+        // FIRST ADD THE PARTICLES
+
         particlesNode = ParticlesNode()
 
         particlesNode.loadParticles(type: .Snow)
 
         addChild(particlesNode)
+
+        // THEN ADD A VIEW (WALL WITH TRASPARENT WINDOWS FOR EX.)
+
+        wall = SKSpriteNode(imageNamed: "Square")
+
+        wall.size = size
+        wall.position = CGPointMake(CGRectGetMidX(frame), CGRectGetMidY(frame))
+
+        addChild(wall)
     }
 
     override func didMoveToView(view: SKView)
     {
-        loadParticles(type: .Snow)
+        // While we can call this method here, if we put all asets loading methods in here, the level wil take time to load/appear, and some animations won't be finished, requiring a Loading Screen
+        // It's also possible to prefetch all textures and create all nodes by calling the method from the parent view, or using the Atlas preload methods
+
+        //loadParticles(type: .Snow)
     }
 
 
@@ -38,5 +53,8 @@ class GameScene: SKScene
             /* unless their parent node has constraints or in a convenience method such as update(frame:) */
             particlesNode.position = CGPointMake(CGRectGetMidX(frame), CGRectGetMaxY(frame))
         }
+
+        wall.size = size
+        wall.position = CGPointMake(CGRectGetMidX(frame), CGRectGetMidY(frame))
     }
 }
